1620f46713be9744a8a40602d8961a5d15117644
Leonardo Alves <leonardo.tenorio@outlook.com>
2023-03-26 20:35:11 -0300

    Bad solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -5,13 +5,13 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class Reservation {
-	
+
 	private Integer roomNumber;
 	private Date checkIn;
 	private Date checkOut;
-	
+
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-	
+
 	public Reservation() {
 	}
 
@@ -36,27 +36,31 @@ public class Reservation {
 	public Date getCheckOut() {
 		return checkOut;
 	}
-	
+
 	public long duration() {// o calculo a ser usado retorna o tipo long, por isso não usar int
 		long diff = checkOut.getTime() - checkIn.getTime();// pega a diferença em milisegundos
-		return TimeUnit.DAYS.convert(diff,TimeUnit.MILLISECONDS);//Usa-se essa classe TimeUnit para converter para long
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);// Usa-se essa classe TimeUnit para converter para
+																	// long
 	}
-	
-	public void updateDates (Date checkIn, Date checkOut) {
+
+	public String updateDates(Date checkIn, Date checkOut) {
+
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			return "Reservation dates for update must be future dates";
+		} // Usa-se um if após o outro pq o primeiro interrompe o código se a condição for
+			// verdade
+		if (!checkOut.after(checkIn)) {
+			return "Check-out date must be after check-in date";
+		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		return null; // Faz-se desta forma pois se não ocorrer erro precisa retornar uma String, então retorna vazio
 	}
-		
+
 	@Override
 	public String toString() {
-		return "Room "
-				+ roomNumber
-				+ " , check-in: "
-				+ sdf.format(checkIn)
-				+ " , check-out: "
-				+ sdf.format(checkOut)
-				+ " , "
-				+ duration()
-				+ " nights";
+		return "Room " + roomNumber + " , check-in: " + sdf.format(checkIn) + " , check-out: " + sdf.format(checkOut)
+				+ " , " + duration() + " nights";
 	}
 }
